9b71dc7a604c388b17b178d671e87afbbdf5f580
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2026-09-14 14:55:20 +0100

    add error handling and utf-8 encoding to cities extraction

--- extraction/cities.py
+++ extraction/cities.py
@@ -2,6 +2,25 @@ import requests
 
 url = "https://simplemaps.com/static/data/country-cities/ma/ma.csv"
 
-response = requests.get(url)
-with open("data/bronze/cities/ma_cities.csv", "w", encoding="utf-8") as file:
-    file.write(response.text)
+
+try:
+    response = requests.get(url , timeout=10)
+    response.raise_for_status()
+    response.encoding = "utf-8"
+
+    print("request successfull")
+    with open("data/bronze/cities/ma_cities.csv" , "w" , encoding="utf_8") as file:
+        file.write(response.text)
+
+except requests.exceptions.RequestException as e:
+    print(f"Request failed : {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+
+
+
+
+
+
+
