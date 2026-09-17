c7b90b721416dc19da32dbc012283300d472efe4
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2026-09-17 13:30:45 +0100

    add weather loading function to postgres.py

--- load/postgres.py
+++ load/postgres.py
@@ -29,3 +29,33 @@ def load_cities():
         print("Cities file is empty.")
 
     except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def load_weather():
+    try:
+        weather_df = pd.read_csv("data/silver/weather_clean.csv")
+
+        weather_df["date"] = pd.to_datetime(weather_df["date"]).dt.date
+
+        weather_df.to_sql(
+            "weather_forecasts",
+            engine,
+            if_exists="append",
+            index=False
+        )
+
+        print(f"{len(weather_df)} weather forecasts loaded successfully.")
+
+    except FileNotFoundError:
+        print("Weather file not found.")
+
+    except pd.errors.EmptyDataError:
+        print("Weather file is empty.")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    load_weather()
